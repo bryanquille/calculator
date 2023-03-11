d54617348a9813417e8c9aa160853f6bae8f6dbf
Bryan Quille <bryanquille@outlook.com>
2023-03-11 17:49:41 -0500

    Giving functionality to operations

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 const numbers = document.querySelectorAll(".number")
 const screen = document.getElementById("screen");
 const cSign = document.getElementById("c--sign");
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.getElementById("equal--sign");
+
+
 const percentSign = document.getElementById("percent--sign");
 const powerSign = document.getElementById("power--sign");
 const divisionSign = document.getElementById("division--sign");
@@ -20,27 +24,78 @@ const plusSign = document.getElementById("plus--sign");
 const zeroSign = document.getElementById("zero--sign");
 const dotSign = document.getElementById("dot--sign");
 const piSign = document.getElementById("pi--sign");
-const equalSign = document.getElementById("equal--sign");
 
+
+let e = 0;
+let dotAfterOper = 0;
 // Showing on screen
 const numbersArray = Array.from(numbers);
 numbersArray.forEach((number) => {
     number.addEventListener("click", () => {
+        
+        // Cleaning after click on equal sign
+        if (e == 1) {
+            screen.textContent = "";
+            e = 0;
+        }
+
+        // Adding value to dot button
         if (number.textContent === ".") {
             number.value = ".";
         }
+
+        // Cleaning the screen
         if (screen.textContent == "Screen") {
             screen.textContent = "";
         }
-        if (number.textContent === "." && screen.textContent.includes(".")) {
+
+        // Verifing not reapeat dot
+        if (number.textContent === "." && screen.textContent.includes(".") && dotAfterOper == 0) {
             number.value = "";
+        } else if (number.textContent === "." && dotAfterOper == 1) {
+            number.value = ".";
+            dotAfterOper = 0;
         }
+
+        // Concatenating on screen
         screen.textContent += number.value;
     });
 });
 
+// Clear screen
 cSign.addEventListener("click", () => {
     screen.textContent = "Screen";
+    e = 0;
+});
+
+// Concatenating operators
+const operatorsArray = Array.from(operators);
+operatorsArray.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        if (screen.textContent == "Screen") {
+            screen.textContent = "";
+        }
+        screen.textContent += operator.value;
+        dotAfterOper = 1;
+        e = 0;
+    });
+});
+
+// Showing constants
+piSign.addEventListener("click", () => {
+    if (screen.textContent == "Screen") {
+        screen.textContent = "";
+    }
+    piSign.value = Math.PI.toFixed(5);
+    screen.textContent += piSign.value;
+    e = 0;
+});
+
+
+// Evaluating operations
+equalSign.addEventListener("click", () => {
+    screen.textContent = eval(screen.textContent);
+    e = 1;
 });
 
 
